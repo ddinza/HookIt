5e903c83f066cbb4e80b927fac35e6bbd5a2611e
dionny <dionny.dinza@icloud.com>
2026-06-24 21:14:52 -0400

    Initial Commit

---
+++ HookIt/HookItApp.swift
@@ -0,0 +1,17 @@
+//
+//  HookItApp.swift
+//  HookIt
+//
+//  Created by Dionny Dinza on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct HookItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
